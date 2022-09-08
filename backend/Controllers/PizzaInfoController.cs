cfcd8e90996f2824f2e429eef3304048bb5b2de5
Tamer Abdelsalam <t.abdelsalam@easternenterprise.com>
2022-09-08 16:33:05 +0200

    making namespace file scoped

--- backend/Controllers/PizzaInfoController.cs
+++ backend/Controllers/PizzaInfoController.cs
@@ -8,33 +8,32 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Caching.Distributed;
 using System.Text.Json;
 
-namespace backend.Controllers
+namespace backend.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PizzaInfoController : ControllerBase
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class PizzaInfoController : ControllerBase
+    private static readonly PizzaInfo[] TheMenu = new[]
     {
-        private static readonly PizzaInfo[] TheMenu = new[]
-        {
-            new PizzaInfo { PizzaName = "The Mighty Meatball", Ingredients = "Meatballs and cheese", Cost = 40, InStock = "yes"},
-            new PizzaInfo { PizzaName = "Crab Apple", Ingredients = "Dungeness crab and apples", Cost = 35, InStock = "no"},
-            new PizzaInfo { PizzaName = "Forest Floor", Ingredients = "Mushrooms, rutabagas, and walnuts", Cost = 20, InStock = "yes"},
-            new PizzaInfo { PizzaName = "Don't At Me", Ingredients = "Pineapple, Canadian bacon, jalapeños", Cost = 25, InStock = "yes"},
-            new PizzaInfo { PizzaName = "Vanilla", Ingredients = "Sausage and pepperoni", Cost = 15, InStock = "no"},
-            new PizzaInfo { PizzaName = "Spice Coming At Ya", Ingredients = "Peppers, chili sauce, spicy andouille", Cost = 50, InStock = "yes"}
-        };
+        new PizzaInfo { PizzaName = "The Mighty Meatball", Ingredients = "Meatballs and cheese", Cost = 40, InStock = "yes"},
+        new PizzaInfo { PizzaName = "Crab Apple", Ingredients = "Dungeness crab and apples", Cost = 35, InStock = "no"},
+        new PizzaInfo { PizzaName = "Forest Floor", Ingredients = "Mushrooms, rutabagas, and walnuts", Cost = 20, InStock = "yes"},
+        new PizzaInfo { PizzaName = "Don't At Me", Ingredients = "Pineapple, Canadian bacon, jalapeños", Cost = 25, InStock = "yes"},
+        new PizzaInfo { PizzaName = "Vanilla", Ingredients = "Sausage and pepperoni", Cost = 15, InStock = "no"},
+        new PizzaInfo { PizzaName = "Spice Coming At Ya", Ingredients = "Peppers, chili sauce, spicy andouille", Cost = 50, InStock = "yes"}
+    };
 
-        private readonly ILogger<PizzaInfoController> _logger;
+    private readonly ILogger<PizzaInfoController> _logger;
 
-        public PizzaInfoController(ILogger<PizzaInfoController> logger)
-        {
-            _logger = logger;
-        }
-
-         [HttpGet]
-         public IEnumerable<PizzaInfo> Get()
-         {
-             return TheMenu;
-         }
+    public PizzaInfoController(ILogger<PizzaInfoController> logger)
+    {
+        _logger = logger;
     }
+
+     [HttpGet]
+     public IEnumerable<PizzaInfo> Get()
+     {
+         return TheMenu;
+     }
 }
